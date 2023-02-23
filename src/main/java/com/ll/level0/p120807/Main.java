90defc3d82b7fe55c61e22cbe12e5cccb768b7f8
nagiltae <nagt1997@gmail.com>
2023-02-23 09:36:28 +0900

    문제풀이

---
+++ src/main/java/com/ll/level0/p120807/Main.java
@@ -0,0 +1,15 @@
+package com.ll.level0.p120807;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(2,3)); //-1
+
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1==num2){return 1;}
+        else{return -1;}
+    }
+}
